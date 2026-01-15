a614b22ca7c961b3f313255d09b5fcf6206f15f7
JuanPabloCesarini <juanpcesarini@hotmail.com>
2026-01-15 18:13:06 -0300

    uso de funciones de flecha

--- 01-reforzamiento/src/bases/05-functions.ts
+++ 01-reforzamiento/src/bases/05-functions.ts
@@ -15,6 +15,11 @@ const greetArrow = (name: string): string => {
     return `Hey, ${name}!`;
 }
 
+// Función flecha simplificada que recibe un nombre y devuelve un saludo informal
+// sin llaves ni return
+
+const greetArrowSimple = (name: string): string => `Hey, ${name}!`; //
+
 const user: User = {
   id: 1,
   username: "john_doe",
@@ -24,7 +29,18 @@ const mostrarUsuario = (user: User): string => {
   return `User ID: ${user.id}, Username: ${user.username}`;
 }
 
+const mostrarUsuarioSimplificado = (user: User): string => `User ID: ${user.id}, Username: ${user.username}`;
+// Función flecha simplificada que recibe un objeto User y devuelve una cadena con su información
 const greeting = greet("World");
-console.log(greeting, greetArrow("Buddy"));
+console.log(greeting, greetArrow("Buddy"), greetArrowSimple("Pal"));
 
-console.log(mostrarUsuario(user));
+console.log(mostrarUsuario(user), mostrarUsuarioSimplificado(user));
+
+const MyNumbers: number[] = [1, 2, 3, 4, 5];
+
+/* MyNumbers.forEach(function (value) {
+  console.log({value}); 
+}); */
+
+//! funcion callback con funcion flecha 
+MyNumbers.forEach( (value) => console.log({value}) );
