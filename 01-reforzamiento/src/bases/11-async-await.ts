f69d5670aa16b8d0915e46b0639673cff9384a58
JuanPabloCesarini <juanpcesarini@hotmail.com>
2026-01-17 19:46:32 -0300

    async - await

--- 01-reforzamiento/src/bases/11-async-await.ts
+++ 01-reforzamiento/src/bases/11-async-await.ts
@@ -11,7 +11,7 @@ const createImageInsideDOM = (url: string) => {
   imgElement.src = url;
   document.body.append(imgElement);
 };
-myRequest
+/* myRequest
   .then((response) => response.json())
   .then(({ data }: GiphyRandomResponse) => {
     const imageUrl = data.images.original.url;
@@ -19,4 +19,16 @@ myRequest
   })
   .catch((err) => {
     console.error(err);
-  });
+  }); */
+
+const getRandomGifUrl = async () => {
+  const response = await fetch(
+    `https://api.giphy.com/v1/stickers/random?api_key=${API_KEY}`,
+  );
+
+  const {data} = (await response.json()) as GiphyRandomResponse;
+  console.log(data.images.original.url)
+  return data.images.original.url;
+}
+
+getRandomGifUrl().then((url)=>createImageInsideDOM(url))
--- 01-reforzamiento/src/main.ts
+++ 01-reforzamiento/src/main.ts
@@ -10,7 +10,8 @@ import './style.css'
 //import './bases/08-imp-exp';
 //import { Owner } from './bases/data/heroes.data';
 //import './bases/09-promises';
-import './bases/10-fetch-api';
+//import './bases/10-fetch-api';
+import './bases/11-async-await';
 
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
   <div>
