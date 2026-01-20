bf73845d1fde783aa46c828ebb3b2e0efa07db64
JuanPabloCesarini <juanpcesarini@hotmail.com>
2026-01-20 16:51:57 -0300

    uso de describe() para agrupar pruebas

--- 02-first-steps/src/helpers/math.helper.test.ts
+++ 02-first-steps/src/helpers/math.helper.test.ts
@@ -1,13 +1,36 @@
-import { expect, test } from "vitest";
-import { add } from "./math.helper";
+import { describe, expect, test } from "vitest";
+import { add, multiply, subtract } from "./math.helper";
 
-test("should add two positives numbers", () => {
-  //! 1. Arrqange
-  const a = 1;
-  const b = 2;
-  //! 2. Act
-  const res = add(a, b);
-  //! 3. Assert
+describe("calculator", () => {
+  test("add two positives numbers", () => {
+    //! 1. Arrqange
+    const a = 1;
+    const b = 2;
+    //! 2. Act
+    const res = add(a, b);
+    //! 3. Assert
 
-  expect(res).toBe(a +b);
+    expect(res).toBe(a + b);
+  });
+
+    test("subtract two positives numbers", () => {
+    //! 1. Arrqange
+    const a = -5;
+    const b = -2;
+    //! 2. Act
+    const res = subtract(a, b);
+    //! 3. Assert
+
+    expect(res).toBe(a - b);
+  });
+      test("multiply two positives numbers", () => {
+    //! 1. Arrqange
+    const a = 1;
+    const b = 2;
+    //! 2. Act
+    const res = multiply(a, b);
+    //! 3. Assert
+
+    expect(res).toBe(a * b);
+  });
 });
