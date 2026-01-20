0e87ab98519c6b6fcc01a8126b76708826042c84
JuanPabloCesarini <juanpcesarini@hotmail.com>
2026-01-20 09:25:55 -0300

    impresión de variables

--- 02-first-steps/src/MyAwesomeApp.tsx
+++ 02-first-steps/src/MyAwesomeApp.tsx
@@ -1,8 +1,22 @@
+const firstName = "Juan Pablo";
+const lastName = " Cesarini";
+const favoritesGames = ["Damas", "Dados", "Burako"];
+const isActive = true;
+const address = {
+  zipCode: "ABC-123",
+  conuntry: "mi_país",
+};
+
 export function MyAwosomeApp() {
-    return (
-        <>
-            <h1>Juan Pablo</h1>
-         <h3>Cesarini</h3>
-        </>
-    )
+  return (
+    <>
+      <h1>{firstName}</h1>
+      <h3>{lastName}</h3>
+      <p>{favoritesGames.join(", ")}</p>
+
+      <h1>{isActive ? "Activo" : "No activo"}</h1>
+
+      <p>{JSON.stringify(address)}</p>
+    </>
+  );
 }
