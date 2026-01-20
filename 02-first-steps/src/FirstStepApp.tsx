05e1e267f250b410ecf1ec23aa93d3ba99699950
JuanPabloCesarini <juanpcesarini@hotmail.com>
2026-01-20 10:57:45 -0300

    Mostrar listado de elementos

--- 02-first-steps/src/FirstStepApp.tsx
+++ 02-first-steps/src/FirstStepApp.tsx
@@ -1,12 +1,26 @@
 import { ItemCounter } from "./shopping-cart/ItemCounter";
 
+interface ItemInCart {
+  productDescription: string;
+  quantity: number;
+}
+
+const itemsInCart: ItemInCart[] = [
+  { productDescription: "Producto A", quantity: 1 },
+  { productDescription: "Producto B", quantity: 4 },
+  { productDescription: "Producto C", quantity: 3 },
+];
 export function FirstStepApp() {
-    return (
-        <>
-            <h1>Carrito de compras</h1>
-         <ItemCounter productName="Producto 1" quantity={5}/>
+  return (
+    <>
+      <h1>Carrito de compras</h1>
+      {itemsInCart.map(({productDescription, quantity}) => (
+        <ItemCounter key={productDescription} productName={productDescription} quantity={quantity} />
+      ))}
+
+      {/*          <ItemCounter productName="Producto 1" quantity={5}/>
          <ItemCounter productName="Producto 2" quantity={3}/>
-         <ItemCounter productName="Producto 3" quantity={2}/>
-        </>
-    )
+         <ItemCounter productName="Producto 3" quantity={2}/> */}
+    </>
+  );
 }
