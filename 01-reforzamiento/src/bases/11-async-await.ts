3fa93524a14fa5ec9053f2c9a44bb33172760af0
JuanPabloCesarini <juanpcesarini@hotmail.com>
2026-01-20 09:39:04 -0300

    corrigiendo un warning}

--- 01-reforzamiento/src/bases/11-async-await.ts
+++ 01-reforzamiento/src/bases/11-async-await.ts
@@ -2,9 +2,9 @@ import type { GiphyRandomResponse } from "./data/giphy.response";
 
 const API_KEY = "Kf1dg0GwYfzmY9MstNNeVmQrvv2TEZb5";
 
-const myRequest = fetch(
+/* const myRequest = fetch(
   `https://api.giphy.com/v1/stickers/random?api_key=${API_KEY}`,
-);
+); */
 
 const createImageInsideDOM = (url: string) => {
   const imgElement = document.createElement("img");
