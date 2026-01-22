8e2c6a097cdae9fc101ad9a3e49e8d5804b891ff
JuanPabloCesarini <juanpcesarini@hotmail.com>
2026-01-22 08:49:56 -0300

    componentes ficticios - Mock Components

--- 02-first-steps/src/FirstStepApp.test.tsx
+++ 02-first-steps/src/FirstStepApp.test.tsx
@@ -1,10 +1,25 @@
-import { describe, expect, test } from "vitest";
+import { describe, expect, test, vi } from "vitest";
 import { FirstStepApp } from "./FirstStepApp";
-import { render } from "@testing-library/react";
+import { render,screen } from "@testing-library/react";
+
+
+vi.mock('./shopping-cart/ItemCounter',()=>({
+    ItemCounter: ()=> <div data-testid="ItemCounter"/>
+}));
+
 
 describe('FirstStepApp',()=>{
     test('should match snapshot',()=>{
         const container = render(<FirstStepApp/>)
         expect(container).toMatchSnapshot();
+    })
+
+    test('should render the correct number of ItemCounter components',()=>{
+        render(<FirstStepApp/>)
+
+        const ItemCounters =screen.getAllByTestId('ItemCounter');
+
+        expect(ItemCounters.length).toBe(3);
+
     })
 })
