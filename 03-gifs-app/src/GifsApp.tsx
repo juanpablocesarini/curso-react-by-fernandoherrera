b94bfc4b52420cc23dc6291a1493b8b46df0cdb6
JuanPabloCesarini <juanpcesarini@hotmail.com>
2026-01-23 18:07:55 -0300

    fin seccion 6

--- 03-gifs-app/src/GifsApp.tsx
+++ 03-gifs-app/src/GifsApp.tsx
@@ -1,13 +1,15 @@
 import { useState } from "react";
 import { GifsList } from "./gifs/components/GifsList";
 import { PreviousSearches } from "./gifs/components/PreviousSearches";
-import { mockGifs } from "./mock-data/gifs.mock";
 import { CustomHeader } from "./shared/components/CustomHeader";
 import { SearchBar } from "./shared/components/SearchBar";
 import { getGifsByQuery } from "./gifs/actions/get-gifs-by-query.action";
+import type { Gif } from "./gifs/interfaces/gif.interface";
 
 export const GifsApp = () => {
-  const [previousTerms, setPreviousTerms] = useState(["cinco"]);
+
+  const [gifs, setGifs] = useState<Gif[]>([])
+  const [previousTerms, setPreviousTerms] = useState<string[]>([]);
   const handleTermClicked = (term: string) => {
     console.log({ term });
   };
@@ -20,7 +22,7 @@ export const GifsApp = () => {
 
    const gifs = await getGifsByQuery(query)
 
-   console.log({gifs})
+   setGifs(gifs)
   };
   return (
     <>
@@ -38,7 +40,7 @@ export const GifsApp = () => {
       />
 
       {/* Gifs */}
-      <GifsList gifsList={mockGifs} />
+      <GifsList gifsList={gifs} />
     </>
   );
 };
