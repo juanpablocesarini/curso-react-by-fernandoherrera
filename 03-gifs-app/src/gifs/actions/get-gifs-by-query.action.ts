793e3dc8894ed6f9893c9dcc08450c779cb85fb8
JuanPabloCesarini <juanpcesarini@hotmail.com>
2026-01-23 17:44:17 -0300

    manejo de variables de entorno

--- 03-gifs-app/src/gifs/actions/get-gifs-by-query.action.ts
+++ 03-gifs-app/src/gifs/actions/get-gifs-by-query.action.ts
@@ -1,14 +1,12 @@
-import axios from "axios";
 import type { GiphyResponse } from "../interfaces/giphy.response";
 import type { Gif } from "../interfaces/gif.interface";
+import { giphyApi } from "../api/giphy.api";
 
 export const getGifsByQuery = async (query:string) : Promise<Gif[]>=> {
-    const response = await axios.get<GiphyResponse>('https://api.giphy.com/v1/gifs/search', {
+    const response = await giphyApi<GiphyResponse>('/search', {
         params: {
             q:query,
             limit:10,
-            lang:'es',
-            api_key:'Kf1dg0GwYfzmY9MstNNeVmQrvv2TEZb5'
         }
     });
 
