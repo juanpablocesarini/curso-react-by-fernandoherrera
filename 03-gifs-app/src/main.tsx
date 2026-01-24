2630c3d72bfe505c9bb7e1b73ede52ed52baadac
JuanPabloCesarini <juanpcesarini@hotmail.com>
2026-01-24 12:58:50 -0300

    inicio sección 7 - Custom Hooks

--- 03-gifs-app/src/main.tsx
+++ 03-gifs-app/src/main.tsx
@@ -1,11 +1,14 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import { GifsApp } from './GifsApp';
+import { MyCounterApp } from './counter/components/MyCounterapp'
+/* import { GifsApp } from './GifsApp'; */
+
 
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-   <GifsApp/>
+   {/* <GifsApp/> */}
+   <MyCounterApp/>
   </StrictMode>,
 )
