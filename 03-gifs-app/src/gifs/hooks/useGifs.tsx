862b4a4ca92e1da803b6b2c72eb1e57bfd303d95
JuanPabloCesarini <juanpcesarini@hotmail.com>
2026-01-24 15:27:03 -0300

    fin seccion 7 - useRef - Mantener el valor entre re-renders

--- 03-gifs-app/src/gifs/hooks/useGifs.tsx
+++ 03-gifs-app/src/gifs/hooks/useGifs.tsx
@@ -1,18 +1,18 @@
-import { useState } from "react";
+import { useRef, useState } from "react";
 import type { Gif } from "../interfaces/gif.interface";
 import { getGifsByQuery } from "../actions/get-gifs-by-query.action";
 
-const gifsCache: Record<string, Gif[]> = {};
+//const gifsCache: Record<string, Gif[]> = {};
 
 export const useGifs = () => {
   const [gifs, setGifs] = useState<Gif[]>([]);
   const [previousTerms, setPreviousTerms] = useState<string[]>([]);
 
-
+  const gifsCache = useRef<Record<string, Gif[]>>({});
   const handleTermClicked = async (term: string) => {
-    if(gifsCache[term]){
-        setGifs(gifsCache[term]);
-        return;
+    if (gifsCache.current[term]) {
+      setGifs(gifsCache.current[term]);
+      return;
     }
     const gifs = await getGifsByQuery(term);
     setGifs(gifs);
@@ -27,8 +27,7 @@ export const useGifs = () => {
     const gifs = await getGifsByQuery(query);
 
     setGifs(gifs);
-
-    gifsCache[query] =gifs;
+    gifsCache.current[query] = gifs;
   };
   return {
     gifs,
