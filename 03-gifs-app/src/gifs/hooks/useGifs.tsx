92d518991b9d23487409872dd74c68ea86cc9241
JuanPabloCesarini <juanpcesarini@hotmail.com>
2026-01-24 14:44:38 -0300

    manejo de cache

--- 03-gifs-app/src/gifs/hooks/useGifs.tsx
+++ 03-gifs-app/src/gifs/hooks/useGifs.tsx
@@ -1,12 +1,21 @@
-import  { useState } from "react";
+import { useState } from "react";
 import type { Gif } from "../interfaces/gif.interface";
 import { getGifsByQuery } from "../actions/get-gifs-by-query.action";
+
+const gifsCache: Record<string, Gif[]> = {};
 
 export const useGifs = () => {
   const [gifs, setGifs] = useState<Gif[]>([]);
   const [previousTerms, setPreviousTerms] = useState<string[]>([]);
-  const handleTermClicked = (term: string) => {
-    console.log({ term });
+
+
+  const handleTermClicked = async (term: string) => {
+    if(gifsCache[term]){
+        setGifs(gifsCache[term]);
+        return;
+    }
+    const gifs = await getGifsByQuery(term);
+    setGifs(gifs);
   };
 
   const handleSearch = async (query: string = "") => {
@@ -18,11 +27,13 @@ export const useGifs = () => {
     const gifs = await getGifsByQuery(query);
 
     setGifs(gifs);
+
+    gifsCache[query] =gifs;
   };
   return {
     gifs,
     previousTerms,
     handleTermClicked,
-    handleSearch
-  }
-}
+    handleSearch,
+  };
+};
