03aa413758a72e3c812a0f1eec050407c9566095
JuanPabloCesarini <juanpcesarini@hotmail.com>
2026-01-27 10:17:17 -0300

    Axios mock adapter - Controlar resultados de Axios

--- 03-gifs-app/src/gifs/actions/get-gifs-by-query.action.test.ts
+++ 03-gifs-app/src/gifs/actions/get-gifs-by-query.action.test.ts
@@ -1,9 +1,14 @@
 import { describe, expect, test } from "vitest";
+import axiosMockAdapter from 'axios-mock-adapter';
 import { getGifsByQuery } from "./get-gifs-by-query.action";
+import { giphyApi } from "../api/giphy.api";
+import { giphySearchResponseMock } from '../../../tests/mocks/giphy.response.data';
 
 
 describe('getGifsByQuery', ()=>{
-    test('should returns a list of gifs', async ()=>{
+    const mock = new axiosMockAdapter(giphyApi);
+
+ /*    test('should returns a list of gifs', async ()=>{
         const gifs = await getGifsByQuery('pikachu');
         const [gif1] = gifs
         
@@ -14,5 +19,21 @@ describe('getGifsByQuery', ()=>{
             width: expect.any(Number),
             url: expect.any(String),
         })
+    }) */
+   test('should returns a list of gifs', async () =>{
+        mock.onGet('/search').reply(200,giphySearchResponseMock);
+
+        const gifs = await getGifsByQuery('pikachu');
+
+    expect(gifs.length).toBe(10);
+
+    gifs.forEach(gif =>{
+        expect(typeof gif.id).toBe('string');
+        expect(typeof gif.title).toBe('string');
+        expect(typeof gif.url).toBe('string');
+        expect(typeof gif.height).toBe('number');
+        expect(typeof gif.width).toBe('number');
+
     })
+   })
 })
