61ce717d1d6900dce25c23d9b998ce31463f17b9
JuanPabloCesarini <juanpcesarini@hotmail.com>
2026-01-28 15:14:56 -0300

    useEffect - disparar efectos

--- 04-hooks-app/src/main.tsx
+++ 04-hooks-app/src/main.tsx
@@ -1,13 +1,15 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 /* import { HookApp } from './HookApp'; */
+//import { TrafficLight } from './01-useState/TrafficLight'
 import './index.css'
-import { TrafficLight } from './01-useState/TrafficLight'
+import { TrafficLightWithEffect } from './02-useEffect/TrafficLightWithEffect'
 
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
    {/* < HookApp/> */}
-   <TrafficLight/>
+   {/* <TrafficLight/> */}
+   <TrafficLightWithEffect/>
   </StrictMode>,
 )
