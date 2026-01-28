e1223846a1c51b13a7e4f09983f8e92596661e03
JuanPabloCesarini <juanpcesarini@hotmail.com>
2026-01-28 15:48:51 -0300

    Más sobre useEffect

--- 04-hooks-app/src/02-useEffect/TrafficLightWithEffect.tsx
+++ 04-hooks-app/src/02-useEffect/TrafficLightWithEffect.tsx
@@ -1,11 +1,9 @@
 import { useEffect, useState } from "react";
 
-
 const colors = {
   red: "bg-red-500 animate-pulse",
   yellow: "bg-yellow-500 animate-pulse",
   green: "bg-green-500 animate-pulse",
-  blue: "bg-blue-500 animate-pulse",
 };
 
 //type TrafficLightColor= 'red'|'yellow'|'green';
@@ -13,26 +11,56 @@ type TrafficLightColor = keyof typeof colors;
 
 export const TrafficLightWithEffect = () => {
   const [light, setLight] = useState<TrafficLightColor>("red");
-  const [countDown, setCountDown] = useState(5)
+  const [countDown, setCountDown] = useState(5);
 
-  useEffect(()=>{
-    if (countDown === 0)return;
+  useEffect(() => {
+    if (countDown === 0) return;
 
-   const setIntervalId = setInterval(()=>{
-      console.log('setInterval llamado')
-      setCountDown(prev =>prev-1)
-    },1000);
+    const setIntervalId = setInterval(() => {
+      setCountDown((prev) => prev - 1);
+    }, 1000);
 
-    return () =>{
-      console.log('cleanup effect');
+    return () => {
       clearInterval(setIntervalId);
-    }
-  },[countDown])
+    };
+  }, [countDown]);
+
+  // Chqange light color effect
+
+  useEffect(() => {
+    if (countDown > 0) return;
+    
+      // eslint-disable-next-line react-hooks/set-state-in-effect
+      setCountDown(5);
+      if (light === "red") {
+        setLight("green");
+        return;
+      }
+      if (light === "yellow") {
+        setLight("red");
+        return;
+      }
+      if (light === "green") {
+        setLight("yellow");
+        return;
+      }
+      
+    
+  }, [countDown, light]);
+
   return (
-    <div className="min-h-screen bg-gradient-to-br from-slate-900 via-gray-900 to-slate-800 flex items-center justify-center p-4">
+    <div className="min-h-screen bg-linear-to-br from-slate-900 via-gray-900 to-slate-800 flex items-center justify-center p-4">
       <div className="flex flex-col items-center space-y-8">
-        <h1 className="text-white text-3xl font-thin">Semáforo con useEffect</h1>
+        <h1 className="text-white text-3xl font-thin">
+          Semáforo con useEffect
+        </h1>
         <h2 className="text-white text-xl">Countdown: {countDown}</h2>
+        <div className="w-64 bg-gray-700 rounded-full h-2">
+          <div className="bg-blue-500 h-2 rounded-full transition-all duration-1000 ease-linear"
+          style={{width:`${(countDown/5)*100}%`}}
+          ></div>
+          
+        </div>
         <div
           className={`w-32 h-32
              ${light === "red" ? colors[light] : "bg-gray-500"}
@@ -49,8 +77,6 @@ export const TrafficLightWithEffect = () => {
             ${light === "green" ? colors[light] : "bg-gray-500"} 
             rounded-full`}
         ></div>
-
-     
       </div>
     </div>
   );
