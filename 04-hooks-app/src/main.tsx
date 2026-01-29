ab8298df27adf2563be68b8d6163967c7f8081f9
JuanPabloCesarini <juanpcesarini@hotmail.com>
2026-01-29 08:26:05 -0300

    comienzo sección 10 - trabajamos con useReducer

--- 04-hooks-app/src/main.tsx
+++ 04-hooks-app/src/main.tsx
@@ -2,6 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import { FocusScreen } from './04-useRef/FocusScreen'
+import { TasksApp } from './05-useReducer/TaskApp'
 //import { PokemonPage } from './03-examples/PokemonPage'
 //import { TrafficLightWithHook } from './02-useEffect/TrafficLightWithHook'
 /* import { HookApp } from './HookApp'; */
@@ -16,6 +17,7 @@ createRoot(document.getElementById('root')!).render(
    {/* <TrafficLightWithEffect/> */}
    {/* <TrafficLightWithHook/> */}
    {/* <PokemonPage/> */}
-   <FocusScreen/>
+   {/* <FocusScreen/> */}
+   <TasksApp/>
   </StrictMode>,
 )
