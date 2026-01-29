899e016469caf608ffeff8775b63a8dd631bad2f
JuanPabloCesarini <juanpcesarini@hotmail.com>
2026-01-29 10:28:03 -0300

    configuración de la función reducer

--- 04-hooks-app/src/05-useReducer/reducer/taskReducer.ts
+++ 04-hooks-app/src/05-useReducer/reducer/taskReducer.ts
@@ -19,5 +19,36 @@ export const taskReducer = (
   state: TaskState,
   action: TaskAction,
 ): TaskState => {
-  return state;
+  switch (action.type) {
+    case "ADD_TODO": {
+      const newTodo: Todo = {
+        id: Date.now(),
+        text: action.payload,
+        completed: false,
+      };
+      return {
+        ...state,
+        todos: [...state.todos, newTodo],
+      };
+    }
+    case "DELETE_TODO":
+      return {
+        ...state,
+        todos: state.todos.filter((todo) => todo.id != action.payload),
+      };
+    case "TOGGLE_TODO": {
+      const updatedTodos = state.todos.map((todo) => {
+        if (todo.id === action.payload) {
+          return { ...todo, completed: !todo.completed };
+        }
+        return todo;
+      });
+      return {
+        ...state,
+        todos: updatedTodos,
+      };
+    }
+    default:
+      return state;
+  }
 };
