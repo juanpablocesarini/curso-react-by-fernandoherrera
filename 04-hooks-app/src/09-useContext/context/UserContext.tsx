7f96c921ebdcefe8cb868be76c63de5592d7cdda
JuanPabloCesarini <juanpcesarini@hotmail.com>
2026-01-31 10:36:29 -0300

    Contexto de usuario - Información global

--- 04-hooks-app/src/09-useContext/context/UserContext.tsx
+++ 04-hooks-app/src/09-useContext/context/UserContext.tsx
@@ -1,13 +1,42 @@
-import type { PropsWithChildren } from "react"
+import { createContext, useState, type PropsWithChildren } from "react";
+import type { User } from "../data/user-mock.data";
 
+type AuthStatus = "checking" | "authenticated" | "not-authenticated";
 
+interface UserContextProps {
+  //state
+  authStatus: AuthStatus;
+  user: User | null;
 
-export const UserContextProvider = ({children}:PropsWithChildren) => {
-
-
-  return (
-    <div>
-        {children}
-    </div>
-  )
+  // methods
+  login: (userId: number) => boolean;
+  logout: () => void;
 }
+
+export const UserContext = createContext({} as UserContextProps);
+
+export const UserContextProvider = ({ children }: PropsWithChildren) => {
+  const [authStatus, setAuthStatus] = useState<AuthStatus>("checking");
+  const [user, setUser] = useState<User | null>(null);
+
+  const handleLogin = (userId: number) => {
+    console.log({ userId });
+    return true;
+  };
+
+  const handleLogout = () => {
+    console.log('logout');
+  };
+  return (
+    <UserContext
+      value={{
+        authStatus: authStatus,
+        user: user,
+        login: handleLogin,
+        logout:handleLogout
+      }}
+    >
+      {children}
+    </UserContext>
+  );
+};
