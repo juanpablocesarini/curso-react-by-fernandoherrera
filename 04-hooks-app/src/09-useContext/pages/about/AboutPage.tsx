7b4082c002bcbf55948207f6cc5e0d93dc89533b
JuanPabloCesarini <juanpcesarini@hotmail.com>
2026-01-31 09:48:43 -0300

    Navegar entre pantallas - Diseño de páginas

--- 04-hooks-app/src/09-useContext/pages/about/AboutPage.tsx
+++ 04-hooks-app/src/09-useContext/pages/about/AboutPage.tsx
@@ -1,7 +1,14 @@
-
+import { Link } from "react-router";
 
 export const AboutPage = () => {
   return (
-    <div>AboutPage</div>
-  )
-}
+    <div className="flex flex-col items-center justify-center min-h-screen">
+      <h1 className="text-4xl font-bold"> Sobre mi</h1>
+      <hr />
+      <div className="flex flex-col gap 2" >
+        <Link to="/profile" className="hover:text-blue-500 underline text-xl">Perfil</Link>
+        <Link to="/login" className="hover:text-blue-500 underline text-xl">Iniciar Sesion</Link>
+      </div>
+    </div>
+  );
+};
--- 04-hooks-app/src/09-useContext/pages/auth/LoginPage.tsx
+++ 04-hooks-app/src/09-useContext/pages/auth/LoginPage.tsx
@@ -1,7 +1,20 @@
+import { Button } from '@/components/ui/button';
+import { Input } from '@/components/ui/input';
+import { Link } from 'react-router';
 
 
 export const LoginPage = () => {
   return (
-    <div>LoginPage</div>
+    <div className="flex flex-col items-center min-h-screen">
+        <h1 className="text-4xl font-bold">Iniciar Sesión</h1>
+        <hr />
+        <form className='flex flex-col gap-2 my-10'>
+            <Input type="number" placeholder='ID del usuario'/>
+            <Button type="submit">login</Button>
+        </form>
+        <Link to="/about">
+            <Button variant="ghost">Volver a la página principal</Button>
+        </Link>
+    </div>
   )
 }
