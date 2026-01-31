b99847eca53cb80fab7afdb1414181cda9ed5649
JuanPabloCesarini <juanpcesarini@hotmail.com>
2026-01-31 10:06:05 -0300

    Contextos y proveedores en React

---
+++ 04-hooks-app/src/09-useContext/context/UserContext.tsx
@@ -0,0 +1,13 @@
+import type { PropsWithChildren } from "react"
+
+
+
+export const UserContextProvider = ({children}:PropsWithChildren) => {
+
+
+  return (
+    <div>
+        {children}
+    </div>
+  )
+}
