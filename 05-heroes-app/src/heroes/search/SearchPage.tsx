b1490fb90720d1ba8346b20789e36f3a48fd419e
JuanPabloCesarini <juanpcesarini@hotmail.com>
2026-02-01 15:39:59 -0300

    armando componentes parte2/2

--- 05-heroes-app/src/heroes/search/SearchPage.tsx
+++ 05-heroes-app/src/heroes/search/SearchPage.tsx
@@ -1,11 +1,16 @@
 import { CustomJumbotron } from "@/components/custom/CustomJumbotron";
-
+import { HeroStats } from "../components/HeroStats";
 
 export const SearchPage = () => {
   return (
-   <>
-   <CustomJumbotron title="Búsqueda de superhéroes" description="Descubre. explora y administra superhéroes y villanos"/>
-   </>
-  )
-}
+    <>
+      <CustomJumbotron
+        title="Búsqueda de superhéroes"
+        description="Descubre. explora y administra superhéroes y villanos"
+      />
+      {/* Stats Dashboard */}
+      <HeroStats />
+    </>
+  );
+};
 export default SearchPage;
