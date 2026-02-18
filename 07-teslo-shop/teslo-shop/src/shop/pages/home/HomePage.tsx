ca8a60760a35771c19f927016c11f3689cea1049
JuanPabloCesarini <juanpcesarini@hotmail.com>
2026-02-18 09:03:07 -0300

    TesloShop - HomePage - Paginación y Productos

--- 07-teslo-shop/teslo-shop/src/shop/pages/home/HomePage.tsx
+++ 07-teslo-shop/teslo-shop/src/shop/pages/home/HomePage.tsx
@@ -1,5 +1,8 @@
 
+import { CustomPagination } from "@/components/custom/CustomPagination"
+import { products } from "@/mocks/products.mock"
 import { CustomJumbotron } from "@/shop/components/CustomJumbotron"
+import { ProductsGrid } from "@/shop/components/ProductsGrid"
 
 
 export const HomePage = () => {
@@ -7,6 +10,8 @@ export const HomePage = () => {
   return (
     <>
     <CustomJumbotron title="Todos los productos" />
+    <ProductsGrid products={products}/>
+    <CustomPagination totalPages={7}/>
     </>
   )
 }
