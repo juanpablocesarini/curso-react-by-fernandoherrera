e60861e78f4403efd33fa7efd7f869183fac1b77
JuanPabloCesarini <juanpcesarini@hotmail.com>
2026-02-20 08:12:15 -0300

    Teslo Shop - Lógica entre pantallas

--- 07-teslo-shop/teslo-shop/src/shop/pages/gender/GenderPage.tsx
+++ 07-teslo-shop/teslo-shop/src/shop/pages/gender/GenderPage.tsx
@@ -1,6 +1,23 @@
+import { CustomPagination } from "@/components/custom/CustomPagination"
+import { products } from "@/mocks/products.mock"
+import { CustomJumbotron } from "@/shop/components/CustomJumbotron"
+import { ProductsGrid } from "@/shop/components/ProductsGrid"
+import { useParams } from "react-router"
 
 export const GenderPage = () => {
+
+  const {gender}= useParams();
+
+  const genderLabel = gender === 'men'
+    ? 'Hombres'
+    : gender === 'women'
+      ? 'Muejeres'
+      :'Niños'
   return (
-    <div>GenderPage</div>
+       <>
+       <CustomJumbotron title={`Productos para ${genderLabel}`} />
+       <ProductsGrid products={products}/>
+       <CustomPagination totalPages={7}/>
+       </>
   )
 }
